949670a0569d7e418f47af80b5b29216914ad396
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-10-14 13:14:19 +0200

    Changement du port et message en français

--- docker_web_app/server.js
+++ docker_web_app/server.js
@@ -3,15 +3,15 @@
 const express = require("express");
 
 // Constants
-const PORT = 8080;
+const PORT = 3478;
 const HOST = "0.0.0.0";
 
 // App
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Bonjour");
 });
 
 app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
+  console.log(`Tourne sur http://${HOST}:${PORT}`);
 });
